a5884c8455e067d797d2fea1828ce0e739a231d2
Morvin-Ian <morvinian@gmail.com>
2023-10-04 17:04:49 +0300

    different caches when using docker and not

--- core/settings.py
+++ core/settings.py
@@ -95,16 +95,29 @@ REST_FRAMEWORK = {
       ],
 }
 
+USING_DOCKER = True
 
-CACHES = {
+if USING_DOCKER:
+    CACHES = {
+            "default": {
+                "BACKEND": "django_redis.cache.RedisCache",
+                "LOCATION": "redis://redis:6379/",
+                "OPTIONS": {
+                    "CLIENT_CLASS": "django_redis.client.DefaultClient"
+                },
+            }
+    }
+
+else:
+    CACHES = {
         "default": {
-            "BACKEND": "django_redis.cache.RedisCache",
-            "LOCATION": "redis://redis:6379/",
-            "OPTIONS": {
-                "CLIENT_CLASS": "django_redis.client.DefaultClient"
-            },
+            "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+            "LOCATION": "Blog_Cache_table",
         }
-}
+    }
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
